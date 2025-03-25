9264e3b1a0d07035c38058f801017c42ac35c381
Jithendar12 <jithendar.vallepu@trianz.com>
2025-03-25 11:49:30 +0530

    Add unit tests in handlers package

--- athena-federation-sdk/src/test/java/com/amazonaws/athena/connector/lambda/handlers/CompositeHandlerTest.java
+++ athena-federation-sdk/src/test/java/com/amazonaws/athena/connector/lambda/handlers/CompositeHandlerTest.java
@@ -1,5 +1,3 @@
-package com.amazonaws.athena.connector.lambda.handlers;
-
 /*-
  * #%L
  * Amazon Athena Query Federation SDK
@@ -19,6 +17,7 @@ package com.amazonaws.athena.connector.lambda.handlers;
  * limitations under the License.
  * #L%
  */
+package com.amazonaws.athena.connector.lambda.handlers;
 
 import com.amazonaws.athena.connector.lambda.data.BlockAllocator;
 import com.amazonaws.athena.connector.lambda.data.BlockAllocatorImpl;
@@ -44,8 +43,10 @@ import com.amazonaws.athena.connector.lambda.records.ReadRecordsRequest;
 import com.amazonaws.athena.connector.lambda.records.ReadRecordsResponse;
 import com.amazonaws.athena.connector.lambda.request.PingRequest;
 import com.amazonaws.athena.connector.lambda.request.PingResponse;
+import com.amazonaws.athena.connector.lambda.security.FederatedIdentity;
 import com.amazonaws.athena.connector.lambda.security.IdentityUtil;
 import com.amazonaws.athena.connector.lambda.serde.ObjectMapperFactory;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.ArrowType;
@@ -59,12 +60,20 @@ import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.UUID;
 
 import static com.amazonaws.athena.connector.lambda.domain.predicate.Constraints.DEFAULT_NO_LIMIT;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
@@ -78,6 +87,7 @@ public class CompositeHandlerTest
 
     private MetadataHandler mockMetadataHandler;
     private RecordHandler mockRecordHandler;
+    private UserDefinedFunctionHandler mockUserDefinedFunctionHandler;
     private CompositeHandler compositeHandler;
     private BlockAllocatorImpl allocator;
     private ObjectMapper objectMapper;
@@ -96,6 +106,7 @@ public class CompositeHandlerTest
         objectMapper = ObjectMapperFactory.create(allocator);
         mockMetadataHandler = mock(MetadataHandler.class);
         mockRecordHandler = mock(RecordHandler.class);
+        mockUserDefinedFunctionHandler = mock(UserDefinedFunctionHandler.class);
 
         schemaForRead = SchemaBuilder.newBuilder()
                 .addField("col1", new ArrowType.Int(32, true))
@@ -136,6 +147,13 @@ public class CompositeHandlerTest
     {
         allocator.close();
         logger.info("{}: exit ", testName.getMethodName());
+    }
+
+    @Test
+    public void constructorWithMetadataRecordAndUdfHandler()
+    {
+        CompositeHandler compositeHandler = new CompositeHandler(mockMetadataHandler, mockRecordHandler, mockUserDefinedFunctionHandler);
+        assertNotNull(compositeHandler);
     }
 
     @Test
@@ -225,5 +243,80 @@ public class CompositeHandlerTest
         when(req.getQueryId()).thenReturn("queryId");
         compositeHandler.handleRequest(allocator, req, new ByteArrayOutputStream(), objectMapper);
         verify(mockMetadataHandler, times(1)).doPing(nullable(PingRequest.class));
+    }
+
+    @Test
+    public void handleRequestWithValidInput() throws Exception
+    {
+        String requestData = "{\n" +
+                "          \"@type\": \"ListTablesRequest\",\n" +
+                "          \"identity\": {\n" +
+                "            \"id\": \"UNKNOWN\",\n" +
+                "            \"principal\": \"UNKNOWN\",\n" +
+                "            \"account\": \"12345678910\",\n" +
+                "            \"arn\": \"arn:aws:iam::12345678910:user/test@test.com\",\n" +
+                "            \"tags\": {},\n" +
+                "            \"groups\": []\n" +
+                "          },\n" +
+                "          \"queryId\": \"auto-generated-by-athena\",\n" +
+                "          \"catalogName\": \"testCatalog\",\n" +
+                "          \"schemaName\": \"testSchema\",\n" +
+                "          \"nextToken\": \"testTable\",\n" +
+                "          \"pageSize\": 5\n" +
+                "        }";
+
+        InputStream inputStream = new ByteArrayInputStream(requestData.getBytes());
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Context mockContext = mock(Context.class);
+
+        FederatedIdentity identity = new FederatedIdentity(
+                "arn:aws:iam::12345678910:user/test@test.com",
+                "12345678910",
+                Collections.emptyMap(),
+                Collections.emptyList()
+        );
+        ListTablesRequest expectedRequest = new ListTablesRequest(
+                identity,
+                "auto-generated-by-athena",
+                "testCatalog",
+                "testSchema",
+                "testTable",
+                5
+        );
+
+        when(mockMetadataHandler.doListTables(any(BlockAllocator.class), eq(expectedRequest)))
+                .thenReturn(new ListTablesResponse(
+                        "testCatalog",
+                        Collections.singletonList(new TableName("testSchema", "testTable")),
+                        null
+                ));
+
+        compositeHandler.handleRequest(inputStream, outputStream, mockContext);
+
+        verify(mockMetadataHandler, times(1))
+                .doListTables(any(BlockAllocator.class), eq(expectedRequest));
+
+        String outputData = outputStream.toString();
+        assertNotNull(outputData);
+        assertTrue(outputData.contains("\"tables\":[{\"schemaName\":\"testSchema\",\"tableName\":\"testTable\"}]"));
+    }
+
+    @Test
+    public void handleRequestWithInvalidInput()
+    {
+        String invalidRequestData = "{ \"invalid\": \"data\" }";
+
+        InputStream inputStream = new ByteArrayInputStream(invalidRequestData.getBytes());
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Context mockContext = mock(Context.class);
+
+        Exception exception = assertThrows(RuntimeException.class, () ->
+                compositeHandler.handleRequest(inputStream, outputStream, mockContext)
+        );
+
+        String exceptionMessage = exception.getCause() != null ? exception.getCause().getMessage() : exception.getMessage();
+        assertNotNull(exceptionMessage);
+
+        assertTrue(exceptionMessage.contains("Unexpected field[invalid]"));
     }
 }
