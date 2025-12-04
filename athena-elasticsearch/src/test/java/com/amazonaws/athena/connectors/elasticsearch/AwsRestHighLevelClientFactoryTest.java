cd8a86fe4327d83eda0f25d7cbe8dfe2e1b1f03f
Jithendar12 <jithendar.vallepu@trianz.com>
2025-12-04 20:29:16 +0530

    Add unit tests in Elasticsearch Connector

---
+++ athena-elasticsearch/src/test/java/com/amazonaws/athena/connectors/elasticsearch/AwsRestHighLevelClientFactoryTest.java
@@ -0,0 +1,81 @@
+/*-
+ * #%L
+ * athena-elasticsearch
+ * %%
+ * Copyright (C) 2019 - 2025 Amazon Web Services
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.amazonaws.athena.connectors.elasticsearch;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * This class is used to test the AwsRestHighLevelClientFactory class.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AwsRestHighLevelClientFactoryTest
+{
+    private static final String TEST_ENDPOINT = "https://search-test.us-east-1.es.amazonaws.com";
+    private static final String TEST_ENDPOINT_WITH_CREDENTIALS = "https://myusername@mypassword:www.example.com";
+    private static final String TEST_USERNAME = "testuser";
+    private static final String TEST_PASSWORD = "testpass";
+
+    private AwsRestHighLevelClientFactory factoryWithAwsCredentials;
+    private AwsRestHighLevelClientFactory factoryWithoutAwsCredentials;
+
+    @Before
+    public void setUp()
+    {
+        factoryWithAwsCredentials = new AwsRestHighLevelClientFactory(true);
+        factoryWithoutAwsCredentials = new AwsRestHighLevelClientFactory(false);
+    }
+
+    @Test
+    public void getOrCreateClient_withAwsCredentials_returnsNonNullClient()
+    {
+        AwsRestHighLevelClient client = factoryWithAwsCredentials.getOrCreateClient(TEST_ENDPOINT);
+
+        assertNotNull("Client with AWS credentials should not be null", client);
+    }
+
+    @Test
+    public void getOrCreateClient_withoutAwsCredentials_returnsValidClient()
+    {
+        AwsRestHighLevelClient client = factoryWithoutAwsCredentials.getOrCreateClient(TEST_ENDPOINT);
+
+        assertNotNull("Client without AWS credentials should not be null", client);
+    }
+
+    @Test
+    public void getOrCreateClient_withEmbeddedCredentials_returnsClientWithCredentials()
+    {
+        AwsRestHighLevelClient client = factoryWithoutAwsCredentials.getOrCreateClient(TEST_ENDPOINT_WITH_CREDENTIALS);
+
+        assertNotNull("Client with embedded credentials should not be null", client);
+    }
+
+    @Test
+    public void getOrCreateClient_withUsernameAndPassword_returnsNewClient()
+    {
+        AwsRestHighLevelClient client = factoryWithoutAwsCredentials.getOrCreateClient(TEST_ENDPOINT, TEST_USERNAME, TEST_PASSWORD);
+
+        assertNotNull("Client with username and password should not be null", client);
+    }
+}
